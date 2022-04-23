a351476ae9abbef5be0729d44e49f8da25651b05
Mujahidul Islam <mislam171020@bscse.uiu.ac.bd>
2022-04-24 04:37:00 +0600

    first bad version

---
+++ 278_first_bad_version.py
@@ -0,0 +1,24 @@
+class Solution:
+   def firstBadVersion(self, n: int) -> int:
+        checkAfter=False
+        checkBefore= True
+        start=0
+        end=n
+        while start < end:
+            center= int((start+end)/2)
+            check= isBadVersion(center)
+            if center<=end-1:
+                checkAfter= isBadVersion(center+1)
+            if center>0:
+                checkBefore= isBadVersion(center-1)
+
+            print(check)
+            if check == False and checkAfter == True :
+                return center+1;
+            elif check == True and checkBefore == False:
+                return center
+            elif check == False:
+                start=center
+            else:
+                end=center
+        return -1
